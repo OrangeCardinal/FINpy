94bd7f680a6410f442e9c67b1181ab95b90af560
OrangeCardinal <david.scott.orkin@gmail.com>
2018-11-18 21:07:46 -0700

    wrote the body for the gamma function.

--- options/greeks.py
+++ options/greeks.py
@@ -1,4 +1,4 @@
-from math import sqrt,log,pow, exp
+from math import sqrt,log,pow, exp, e, pi
 from scipy.stats import norm
 
 
@@ -17,7 +17,9 @@ def call_delta(price, strike, vol, r, q, remaining_time):
     :return:
     """
     d1 = (log((price/strike)) + remaining_time * (r - q + pow(vol, 2))) / vol * sqrt(remaining_time)
-    d2 = d1 - vol * sqrt(remaining_time)
+
+    result = exp(-1*q*remaining_time) * norm.cdf(d1)
+    return result
 
 
 def call_price(price, strike, vol, r, q, remaining_time):
@@ -38,8 +40,10 @@ def call_price(price, strike, vol, r, q, remaining_time):
     result = (price * exp(-1*q*remaining_time) * norm.cdf(d1)) - (strike*exp(-1*r*remaining_time)*norm.cdf(d2))
     return result
 
-def put_delta():
-    pass
+def put_delta(price, strike, vol, r, q, remaining_time):
+    d1 = (log((price / strike)) + remaining_time * (r - q + pow(vol, 2))) / vol * sqrt(remaining_time)
+    result = exp(-1*q*remaining_time) * (norm.cdf(d1) - 1)
+    return result
 
 def put_price(price, strike, vol, r, q, remaining_time):
     d1 = (log((price / strike)) + remaining_time * (r - q + pow(vol, 2))) / vol * sqrt(remaining_time)
@@ -48,8 +52,28 @@ def put_price(price, strike, vol, r, q, remaining_time):
     result = (strike*exp(-1*r*remaining_time) * norm.cdf(-1*d2)) - (price * exp(-1*q*remaining_time) * norm.cdf(-1*d1))
     return result
 
-def gamma(strike_price):
-    pass
+def gamma(price, strike, vol, r, q, remaining_time):
+    """
+    Calculate the gamme for an american style option
+
+    :param price:
+    :param strike:
+    :param vol:
+    :param r:
+    :param q:
+    :param remaining_time:
+    :return: gamma
+    """
+    d1 = (log((price / strike)) + remaining_time * (r - q + pow(vol, 2))) / vol * sqrt(remaining_time)
+
+    term1  = pow(e,-1 * q * remaining_time) / (strike * vol * sqrt(remaining_time))
+
+    term2 = 1 / sqrt(2 * pi)
+
+    exponent = -1 * pow(d1,2) / 2
+    term3 = pow(e, exponent)
+    result = term1 * term2 * term3
+    return result
 
 def rho():
     pass
